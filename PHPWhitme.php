5f3c5b50d86a1ce2ca3cfda1fa477483b44a39ae
Rogerio Vicente <rogerpvl@gmail.com>
2009-11-06 13:06:13 +0000

    Simplified code in short() function and corrected a bug found testing. First test added in folder tests.

--- PHPWhitme.php
+++ PHPWhitme.php
@@ -40,27 +40,44 @@ class PHPWhitme
 	
 	/**
 	 * Shorts a list of given urls and notes
-	 * @param mixed $urls all urls to be shortened in 
-	 * 			array or string (in case of only 1 url)
-	 * @param mixed $urlnotes notes of respective urls.
-	 *			Array or string. Must have same size and same type of $urls
-	 * @param string $note the note to be paired with a url
-	 * @param string $hash custom url alias
+	 * @param mixed $urls all urls to be shortened in array or string (in case of only 1 url)
+	 * @param mixed $urlnotes notes of urls. Array or string. Must have same size and same type of $urls
+	 * @param string $note the note to be paired with a url (optional)
+	 * @param string $hash custom url alias (optional)
 	 * @return string the shortened url
 	 */
 	public function short($urls, $urlnotes, $note=null, $hash=null)
 	{
+		
 		if (gettype($urls) != gettype($urlnotes) || count($urls) != count($urlnotes))
 		{
-			throw new Exception('Number of urls must match number of notes.');
+			throw new Exception('Number and type of urls must match number and type of notes.');
 		}
 		
-		$params = is_array($urls) ? 'url='.implode('&url=', $urls) : 'url='.$urls;
-		$params .= is_array($urlnotes) ? '&urlnote='.implode('$urlnote=', $urlnotes) : 'urlnote='.$urlnotes;
+		$params = '';
+		
+		if (is_array($urls))
+		{
+			$params .= 'url='.implode('&url=', array_map('urlencode', $urls));
+		}
+		else
+		{
+			$param .= 'url='.urlencode($urls);
+		}
+		
+		if (is_array($urlnotes))
+		{
+			$params .= '&urlnote='.implode('$urlnote=', array_map('urlencode', $urlnotes));
+		}
+		else
+		{
+			'&urlnote='.urlencode($urlnotes);
+		}
+		
 		$params .= $note !== null ? '&note='.$note : '';
 		$params .= $hash !== null ? '&hash='.$hash : '';
-		
-		return $this->fetch_response(SHORT_URL.urlencode($params));
+
+		return $this->fetch_response(self::SHORT_URL.$params);
 	}
 	
 	/**
